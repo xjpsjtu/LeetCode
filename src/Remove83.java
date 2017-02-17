932d64e4e8adca37e89264bc50d79afb1be8fe81
xjp <xjpsjtu@sjtu.edu.cn>
2017-02-17 22:33:14 +0800

    ...

---
+++ src/Remove83.java
@@ -0,0 +1,50 @@
+public class Remove83 {
+	static class ListNode{
+		int val;
+		ListNode next;
+		ListNode(int val){this.val = val;}
+	}
+	public static void printList(ListNode head){
+		while(head != null){
+			System.out.println(head.val + ",");
+			head = head.next;
+		}
+	}
+	public ListNode deleteDuplicates(ListNode head) {
+        if(head == null)return null;
+        ListNode next = head.next;
+        if(next == null)return head;
+        if(next.val != head.val){
+        	head.next = deleteDuplicates(next);
+        	return head;
+        }else{
+        	while(next.val == head.val){
+        		head = next;
+        		next = next.next;
+        		if(next == null)return head;
+        	}
+        	return deleteDuplicates(head);
+        }
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ListNode l1 = new ListNode(1);
+		ListNode l2 = new ListNode(2);
+		ListNode l3 = new ListNode(2);
+		ListNode l4 = new ListNode(3);
+		ListNode l5 = new ListNode(3);
+		ListNode l6 = new ListNode(3);
+		ListNode l7 = new ListNode(3);
+		l1.next = l2;
+		l2.next = l3;
+		l3.next = l4;
+		l4.next = l5;
+		l5.next = l6;
+		l6.next = l7;
+		l7.next = null;
+		Remove83 r = new Remove83();
+		ListNode l = r.deleteDuplicates(l4);
+		printList(l);
+	}
+
+}
